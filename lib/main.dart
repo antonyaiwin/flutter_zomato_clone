57aaec610394947dc33b553b8a58c4a40b33061b
Antony Aiwin K X <antonyaiwin007@gmail.com>
2024-03-05 01:53:05 +0530

    Completed splash screen

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'utils/theme/theme.dart';
+import 'view/splash_screen/splash_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Zoamto Clone Flutter',
+      themeMode: ThemeMode.system,
+      theme: TAppTheme.lightTheme,
+      darkTheme: TAppTheme.darkTheme,
+      home: const SplashScreen(),
+    );
+  }
+}
---
+++ lib/view/splash_screen/splash_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+import '../../utils/constants/colors.dart';
+import '../login_screen/login_screen.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(const Duration(seconds: 2)).then((value) => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const LoginScreen(),
+        )));
+    return const Scaffold(
+      backgroundColor: ColorConstants.primaryColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'Xomato',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 55,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Eco-Friendly Deliveries',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
