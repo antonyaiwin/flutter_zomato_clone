b3ae2fb7febaba60bd46243b8cc4e406948bdd24
Antony Aiwin K X <antonyaiwin007@gmail.com>
2024-03-10 00:08:53 +0530

    added bottomSheet Theme

---
+++ lib/utils/theme/custom_themes/bottom_sheet_theme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_zomato_clone/utils/constants/colors.dart';
+
+class TBottomSheetTheme {
+  TBottomSheetTheme._();
+  static const BottomSheetThemeData _baseBottomSheetTheme =
+      BottomSheetThemeData();
+
+  static BottomSheetThemeData lightTextTheme = _baseBottomSheetTheme.copyWith(
+    backgroundColor: const Color.fromRGBO(245, 246, 251, 1),
+  );
+
+  static BottomSheetThemeData darkTextTheme = _baseBottomSheetTheme.copyWith(
+    backgroundColor: ColorConstants.black3c,
+  );
+}
--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_zomato_clone/utils/constants/colors.dart';
+import 'package:flutter_zomato_clone/utils/theme/custom_themes/bottom_sheet_theme.dart';
 import 'custom_themes/elevated_button_theme.dart';
 import 'custom_themes/text_theme.dart';
 
@@ -17,12 +18,15 @@ class TAppTheme {
     elevatedButtonTheme: TElevatedButtonTheme.lightElevatedButtonTheme,
     scaffoldBackgroundColor: Colors.white,
     iconTheme: const IconThemeData(color: ColorConstants.primaryColor),
+    bottomSheetTheme: TBottomSheetTheme.lightTextTheme,
   );
 
   static ThemeData darkTheme = _baseTheme.copyWith(
-      brightness: Brightness.dark,
-      textTheme: TTextTheme.darkTextTheme,
-      elevatedButtonTheme: TElevatedButtonTheme.darkElevatedButtonTheme,
-      scaffoldBackgroundColor: Colors.black,
-      iconTheme: const IconThemeData(color: ColorConstants.primaryColor));
+    brightness: Brightness.dark,
+    textTheme: TTextTheme.darkTextTheme,
+    elevatedButtonTheme: TElevatedButtonTheme.darkElevatedButtonTheme,
+    scaffoldBackgroundColor: Colors.black,
+    iconTheme: const IconThemeData(color: ColorConstants.primaryColor),
+    bottomSheetTheme: TBottomSheetTheme.darkTextTheme,
+  );
 }
