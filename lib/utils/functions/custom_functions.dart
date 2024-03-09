ec7e79cf988af7cfa613a14d4e4ffc31d07ecf61
Antony Aiwin K X <antonyaiwin007@gmail.com>
2024-03-10 00:22:15 +0530

    improved custom bottom sheet

--- lib/utils/functions/custom_functions.dart
+++ lib/utils/functions/custom_functions.dart
@@ -1,24 +1,65 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:icons_plus/icons_plus.dart';
 
 void showMyModalBottomSheet({
   required BuildContext context,
-  required Widget child,
   double? height,
   EdgeInsetsGeometry? contentPadding,
+  required ScrollableWidgetBuilder builder,
+  double initialChildSize = /* 0.5 */ 0.66,
+  double minChildSize = 0.25,
+  double maxChildSize = /* 1.0 */ 0.75,
+  bool expand = true,
+  bool snap = /* false */ true,
 }) {
   showModalBottomSheet(
-    shape: const RoundedRectangleBorder(
-      borderRadius: BorderRadius.vertical(
-        top: Radius.circular(15),
-      ),
-    ),
     context: context,
+    elevation: 0,
+    backgroundColor: Colors.transparent,
+    isScrollControlled: true,
     builder: (context) {
-      return Container(
-        height: height,
-        padding: contentPadding ??
-            const EdgeInsets.symmetric(vertical: 40, horizontal: 30),
-        child: child,
+      return GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onTap: () => Navigator.pop(context),
+        child: GestureDetector(
+          onTap: () {},
+          child: DraggableScrollableSheet(
+            initialChildSize: initialChildSize,
+            minChildSize: minChildSize,
+            maxChildSize: maxChildSize,
+            expand: expand,
+            snap: snap,
+            builder: (context, scrollController) => Stack(
+              clipBehavior: Clip.none,
+              children: [
+                Positioned(
+                  top: -60,
+                  left: 0,
+                  right: 0,
+                  child: CircleAvatar(
+                    backgroundColor: CupertinoColors.black.withOpacity(0.7),
+                    radius: 23,
+                    child: const Icon(
+                      MingCute.close_fill,
+                    ),
+                  ),
+                ),
+                Ink(
+                  height: height,
+                  decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.vertical(
+                        top: Radius.circular(15),
+                      ),
+                      color:
+                          Theme.of(context).bottomSheetTheme.backgroundColor),
+                  padding: contentPadding,
+                  child: builder(context, scrollController),
+                ),
+              ],
+            ),
+          ),
+        ),
       );
     },
   );
