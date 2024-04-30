bc2959c45db44afec0142b6f5ca057812c3be70c
Antony Aiwin K X <antonyaiwin007@gmail.com>
2024-05-01 02:20:09 +0530

    added splash image and updated app name

--- lib/view/splash_screen/splash_screen.dart
+++ lib/view/splash_screen/splash_screen.dart
@@ -1,52 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_zomato_clone/utils/constants/image_strings.dart';
 import 'package:flutter_zomato_clone/utils/functions/shared_preferences_utils.dart';
 import 'package:flutter_zomato_clone/view/home_screen/home_screen.dart';
 
-import '../../utils/constants/colors.dart';
 import '../login_screen/login_screen.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void didChangeDependencies() {
+    precacheImage(const AssetImage(kSplashImage), context);
+    precacheImage(const AssetImage(kLoginHeaderImage), context);
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
     navigateToNextScreen(context);
-    return const Scaffold(
-      backgroundColor: ColorConstants.primaryColor,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              'Xomato',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 55,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Text(
-              'Eco-Friendly Deliveries',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-              ),
-            ),
-          ],
-        ),
-      ),
+    return Image.asset(
+      kSplashImage,
+      fit: BoxFit.cover,
+      alignment: Alignment.bottomCenter,
     );
   }
 
   void navigateToNextScreen(BuildContext context) async {
     bool val = await SharedPreferencesUtils.getLoginedUser() == null;
-    Future.delayed(const Duration(seconds: 2))
-        .then((value) => Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(
-              builder: (context) =>
-                  val ? const LoginScreen() : const HomeScreen(),
-            )));
+    Future.delayed(const Duration(seconds: 2)).then(
+      (value) => Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => val ? const LoginScreen() : const HomeScreen(),
+        ),
+      ),
+    );
   }
 }
